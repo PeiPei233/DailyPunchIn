ad4be24d719e3e137e27b8b2c83b8b19988721ac
PeiPei233 <49641967+PeiPei233@users.noreply.github.com>
2022-03-30 00:17:10 +0800

    Update FuckZJUnCovPunchIn.py

--- FuckZJUnCovPunchIn.py
+++ FuckZJUnCovPunchIn.py
@@ -28,81 +28,73 @@ for argv in sys.argv:
         district = argv[1:]
     elif argv[0] == '7' and argv[1:] != '':
         atschool = argv[1:]
-        
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--no-sandbox')
+brower = webdriver.Chrome(chrome_options = chrome_options)
+brower.get('https://healthreport.zju.edu.cn/ncov/wap/default/index')
+#登录
+el = brower.find_element_by_id("username")
+el.send_keys(username)
+
+el = brower.find_element_by_id('password')
+el.send_keys(password)
+
+el.send_keys(Keys.ENTER)
+
+#选择是否在校
+if atschool == 'Yes':
+    el = brower.find_element_by_xpath('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[15]/div/div/div[1]/span[1]')
+    el.click()
+else:
+    el = brower.find_element_by_xpath('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[15]/div/div/div[2]/span[1]')
+    el.click()
+
+#显示所在地选择框
+brower.execute_script("document.getElementsByName(\"ip\")[0].style.display = 'block';")
+
+#选择位置
+el = brower.find_element_by_xpath('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[20]/div/div/select[1]')
+S = Select(el)
+S.select_by_value(province)
+
+el = brower.find_element_by_xpath('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[20]/div/div/select[2]')
+S = Select(el).select_by_value(city)
+
+el = brower.find_element_by_xpath('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[20]/div/div/select[3]')
+S = Select(el).select_by_value(district)
+
+#选择家人是否有恙
+el = brower.find_element_by_xpath('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[22]/div/div/div[1]/span[1]')
+el.click()
+
+#选择个人承诺
+el = brower.find_element_by_xpath('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[34]/div/div/div/span[1]')
+el.click()
+
+#提交
+el = brower.find_element_by_xpath('/html/body/div[1]/div[1]/div/section/div[5]/div/a')
+el.click()
+
+#确认提交
 try:
-    chrome_options = Options()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--no-sandbox')
-    brower = webdriver.Chrome(chrome_options = chrome_options)
-    brower.get('https://healthreport.zju.edu.cn/ncov/wap/default/index')
-    #登录
-    el = brower.find_element_by_id("username")
-    el.send_keys(username)
-
-    el = brower.find_element_by_id('password')
-    el.send_keys(password)
-
-    el.send_keys(Keys.ENTER)
-
-    #选择是否在校
-    if atschool == 'Yes':
-        el = brower.find_element_by_xpath('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[15]/div/div/div[1]/span[1]')
-        el.click()
-    else:
-        el = brower.find_element_by_xpath('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[15]/div/div/div[2]/span[1]')
-        el.click()
-
-    #显示所在地选择框
-    brower.execute_script("document.getElementsByName(\"ip\")[0].style.display = 'block';")
-
-    #选择位置
-    el = brower.find_element_by_xpath('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[20]/div/div/select[1]')
-    S = Select(el)
-    S.select_by_value(province)
-
-    el = brower.find_element_by_xpath('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[20]/div/div/select[2]')
-    S = Select(el).select_by_value(city)
-
-    el = brower.find_element_by_xpath('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[20]/div/div/select[3]')
-    S = Select(el).select_by_value(district)
-
-    #选择家人是否有恙
-    el = brower.find_element_by_xpath('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[22]/div/div/div[1]/span[1]')
+    el = brower.find_element_by_xpath('/html/body/div[3]/div/div[2]/div[2]')
     el.click()
-
-    #选择个人承诺
-    el = brower.find_element_by_xpath('/html/body/div[1]/div[1]/div/section/div[4]/ul/li[34]/div/div/div/span[1]')
-    el.click()
-
-    #提交
-    el = brower.find_element_by_xpath('/html/body/div[1]/div[1]/div/section/div[5]/div/a')
-    el.click()
-
-    #确认提交
-    try:
-        el = brower.find_element_by_xpath('/html/body/div[3]/div/div[2]/div[2]')
-        el.click()
-    except:
-        requests.post(url, json = {
-            "text": {
-                "content":"今日已打卡"
-            },
-            "msgtype":"text"
-        })
-    else:
-        requests.post(url, json = {
-            "text": {
-                "content":"打卡成功"
-            },
-            "msgtype":"text"
-        })
-
-    brower.quit()
 except:
     requests.post(url, json = {
         "text": {
-            "content":"打卡失败"
+            "content":"今日已打卡"
         },
         "msgtype":"text"
     })
+else:
+    requests.post(url, json = {
+        "text": {
+            "content":"打卡成功"
+        },
+        "msgtype":"text"
+    })
+
+brower.quit()
